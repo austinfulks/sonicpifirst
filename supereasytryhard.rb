505ee94908d2cd08850a4e1072b34403d686176c
austinfulks <54602734+austinfulks@users.noreply.github.com>
2020-01-17 15:27:12 -0800

    Add files via upload

---
+++ supereasytryhard.rb
@@ -0,0 +1,19 @@
+# Welcome to Sonic Pi v3.1
+live_loop :tryhard do
+  play :C4
+  sleep 0.25
+  play :E4
+  sleep 0.25
+  play :G4
+  sleep 0.25
+  play :B4
+  sleep 0.25
+  play :C5
+  sleep 0.25
+  play :B4
+  sleep 0.25
+  play :G4
+  sleep 0.25
+  play :E4
+  sleep 0.25
+end
